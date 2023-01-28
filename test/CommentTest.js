e319b4bace244d481be78489284da5e5f805bf3f
Christa Bella Ishimwe <ishimwechristabellah1@gmail.com>
2023-01-29 02:02:31 +0300

    working on Testing all endpoints

--- test/CommentTest.js
+++ test/CommentTest.js
@@ -1,44 +1,91 @@
-const chai = require("chai");
-const expect = require("chai").expect();
-const chaiHttp = require("chai-http");
-const index = require("../src/index");
-const should = chai.should();
-chai.use(chaiHttp);
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const mongoose = require('mongoose');
+const Comment = require('../src/models/CommentsModel');
 
-describe("Testing messages Api", () => {
-  /**
-   * Testing get all Article routes
-   */
-  it("It'll get all the messages", (done) => {
-    chai
-      .request(index)
-      .get("/Api/Articles/all")
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("object");
-        done();
-      });
+describe('createComments', () => {
+    let req, res;
+
+    beforeEach(() => {
+        req = {
+            body: {
+                name: 'Donna',
+                comment: 'Great article!!'
+            },
+            query: {
+                blogId: '63cab7ff8f08b59f051ff475'
+            }
+        };
+        res = {
+            send: sinon.spy(),
+            status: sinon.stub().returns({ json: sinon.spy() })
+        };
+    });
+
+    it('should create a new comment and return it', async () => {
+        await createComments(req, res);
+        expect(res.send.calledOnce).to.be.true;
+        expect(res.status.notCalled).to.be.true;
+        const savedComment = res.send.firstCall.args[0];
+        expect(savedComment).to.be.an.instanceOf(mongoose.Document);
+        expect(savedComment.name).to.equal(req.body.name);
+        expect(savedComment.articleId).to.equal(req.query.blogId);
+        expect(savedComment.comment).to.equal(req.body.comment);
+    });
+
+    it('should return a 500 status code and error message on error', async () => {
+        const error = new Error('Error while saving comment');
+        sinon.stub(Comment.prototype, 'save').throws(error);
+        await createComments(req, res);
+        expect(res.status.calledOnce).to.be.true;
+        expect(res.status.firstCall.args[0]).to.equal(500);
+        expect(res.status().json.calledOnce).to.be.true;
+        expect(res.status().json.firstCall.args[0]).to.deep.equal({ message: error });
+    });
+});
+
+
+describe('getComment', () => {
+  beforeEach(async () => {
+    // Create test data
+    await Comment.create({ articleId: '63cab7ff8f08b59f051ff475', text: 'Test comment 1' });
+    await Comment.create({ articleId: '63d3bad2269a3def3001e576', text: 'Test comment 2' });
   });
-  it("It'll get single message", (done) => {
-    const param = "63ca931d68c1f2d2455eb6e4";
-    chai
-      .request(index)
-      .get("/Api/messages/"+ param)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a("object");
-        done();
-      });
+
+  afterEach(async () => {
+    // Delete test data
+    await Comment.deleteMany();
   });
-   
-  it("It'll delete the message with matching id by authorized user", (done) => {
-    const param = "63ca931d68c1f2d2455eb6e4";
-    chai
-      .request(index)
-      .delete("/Api/messages/message/" + param)
-      .end((err, res) => {
-        res.should.have.status(400);
-        done();
-      });
+
+  it('should return comments for a given article ID', async () => {
+    const req = { params: { articleId: '63cab7ff8f08b59f051ff475' } };
+    const res = {
+      send: (data) => {
+        assert.deepEqual(data, [{ articleId: '163cab7ff8f08b59f051ff475', text: 'Test comment 1' }]);
+      },
+    };
+    await getComment(req, res);
+  });
+
+  it('should return "no comments yet!!" if no comments are found', async () => {
+    const req = { params: { articleId: '63ca9ecfb29252fcbae78962' } };
+    const res = {
+      send: (data) => {
+        assert.deepEqual(data, { message: 'no comments yet!!' });
+      },
+    };
+    await getComment(req, res);
+  });
+
+  it('should return "error happenedddd! sorry" if an error occurs', async () => {
+    // Force an error by passing in an invalid ObjectId
+    const req = { params: { articleId: 'invalidId' } };
+    const res = {
+      send: (data) => {
+        assert.deepEqual(data, { message: 'error happenedddd! sorry' });
+      },
+    };
+    await getComment(req, res);
   });
 });
--- test/ContactTest.js
+++ test/ContactTest.js
@@ -1,44 +1,91 @@
-const chai = require("chai");
-const expect = require("chai").expect();
-const chaiHttp = require("chai-http");
-const index = require("../src/index");
-const should = chai.should();
-chai.use(chaiHttp);
+const assert = require('assert');
+const request = require('supertest');
+const Contact = require('../src/models/contactModel');
 
-describe("Testing messages Api", () => {
-  /**
-   * testing get all blogs routes
-   */
-  it("It will hold all messages from users", (done) => {
-    chai
-      .request(index)
-      .get("/Api/Articles/all")
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("object");
-        done();
-      });
-  });
-  it("It will hold single message", (done) => {
-    const param = "63ca931d68c1f2d2455eb6e4";
-    chai
-      .request(index)
-      .get("/Api/messages/"+ param)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a("object");
-        done();
-      });
-  });
-   
-  it("It should delete the message with matching id by authorized user", (done) => {
-    const param = "63ca931d68c1f2d2455eb6e4";
-    chai
-      .request(index)
-      .delete("/Api/messages/message/" + param)
-      .end((err, res) => {
-        res.should.have.status(400);
-        done();
-      });
-  });
+describe('POST Contact', () => {
+    it('should create a new contact message', (done) => {
+        const contactData = {
+            Email: 'Donna@gmail.com',
+            Subject: 'Test Subject',
+            Message: 'Test message'
+        }
+
+        request(app)
+            .post('api/Contact')
+            .send(contactData)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Contact.findById(res.body._id, (error, contact) => {
+                    if (error) {
+                        return done(error);
+                    }
+
+                    assert.equal(contact.Email, contactData.Email);
+                    assert.equal(contact.Subject, contactData.Subject);
+                    assert.equal(contact.Message, contactData.Message);
+                    done();
+                });
+            });
+    });
+});
+
+
+
+
+describe('Get Contacts', () => {
+    it('should get all contacts with a limit', (done) => {
+        const limit = 5;
+
+        request(app)
+            .get(`api/Contacts?max=3`)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(res.body.length, limit);
+                done();
+            });
+    });
+});
+
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../server');
+const Contact = require('../models/Contact');
+
+describe('Get Contact/:id', () => {
+    it('should get a single contact by id', (done) => {
+        const contact = new Contact({
+            Email: 'bella@gmail.com',
+            Subject: 'Capstone',
+            Message: 'Hi Christa!!, Your Works looks Professional, I would like to get intouch with you via the Email'
+        });
+
+        contact.save((err, contact) => {
+            if (err) {
+                return done(err);
+            }
+
+            request(app)
+                .get(`api/Contact/63ca982b68c1f2d2455eb70`)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    assert.equal(res.body[0]._id, contact._id.toString());
+                    assert.equal(res.body[0].Email, contact.Email);
+                    assert.equal(res.body[0].Subject, contact.Subject);
+                    assert.equal(res.body[0].Message, contact.Message);
+                    done();
+                });
+        });
+    });
 });
